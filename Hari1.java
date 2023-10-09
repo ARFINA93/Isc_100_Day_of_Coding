0a07a64ac45912d424fa1619944d56cb93f8404c
ARFINA93 <finaa5760@gmail.com>
2023-10-09 15:53:42 +0800

    hari_pertama_ngoding

---
+++ Hari1.java
@@ -0,0 +1,26 @@
+package hari1;
+
+public class Hari1 {
+
+    public static void main(String[] args) {
+        String nama = "Arfina";
+     String alamat = "Alu";
+     String nim = "D0221105";
+     char kelas = 'G';
+     String prodi = "informatika";
+     long angkatan = 2021;
+     double tinggi_badan = 160;
+     float berat_badan = 50f;
+     boolean belum_menikah = true;
+     System.out.println("Nama :"+nama);
+     System.out.println("Alamat :"+alamat);
+     System.out.println("Nim :"+nim);
+     System.out.println("Kelas :"+kelas);
+     System.out.println("Prodi :"+prodi);
+     System.out.println("Angkatan :"+angkatan);
+     System.out.println("Tinggi badan :"+tinggi_badan);
+     System.out.println("Berat badan :"+berat_badan);
+     System.out.println("Belum menikah :"+belum_menikah);
+    }
+    
+}
