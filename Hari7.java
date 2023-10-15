fd9bee0b9af02b1a5e034d5f5f4bc170ae85e397
ARFINA93 <118178950+ARFINA93@users.noreply.github.com>
2023-10-15 17:35:40 +0800

    Add files via upload

---
+++ Hari7.java
@@ -0,0 +1,17 @@
+package hari1;
+
+public class Hari7 {
+    public static void main(String[] args) {
+        int nilai = 50;
+        
+        if (nilai >= 100){
+            System.out.println("Anda dapat nilai A");
+        }else if (nilai >= 90){
+            System.out.println("Anda dapat nilai B");
+        }else if (nilai >= 70){
+            System.out.println("Anda dapat nilai C");
+        }else{
+            System.out.println("Anda dapat nilai D");
+        }
+    }
+}
