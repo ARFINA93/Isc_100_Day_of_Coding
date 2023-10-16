5e6718158ad24ec569e7eb2f59ab28a569f92ab2
ARFINA93 <118178950+ARFINA93@users.noreply.github.com>
2023-10-16 20:44:08 +0800

    Add files via upload

---
+++ Hari8.java
@@ -0,0 +1,17 @@
+package hari1;
+
+public class Hari8 {
+    public static void main(String[] args) {
+        
+        int umur_fina = 20;
+        int umur_laelia = 25;
+        
+        //percabangan if-else
+        if (umur_fina < umur_laelia){
+            System.out.println("fina lebih muda"); 
+        }else{
+            System.out.println("laelia lebih tua");
+        }
+    }
+    
+}
