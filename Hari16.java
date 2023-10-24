825d7c3d8f9492730bcbf4120ce70a9d32fd4b07
ARFINA93 <118178950+ARFINA93@users.noreply.github.com>
2023-10-24 23:19:01 +0800

    Add files via upload

---
+++ Hari16.java
@@ -0,0 +1,13 @@
+package hari1;
+
+public class Hari16 {
+    public static void main(String[] args) {
+        //belajar perulangan for
+        int jumlah_perulangan = 10;
+        
+        for(int i = 0; i < jumlah_perulangan; i++){
+            System.out.println("Jangan lupa tidur");
+        }
+    }
+    
+}
