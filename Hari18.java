3232e32df93fd7bf6e14fc9c83163665b7d1f8ad
ARFINA93 <118178950+ARFINA93@users.noreply.github.com>
2023-10-26 23:56:30 +0800

    Add files via upload

---
+++ Hari18.java
@@ -0,0 +1,26 @@
+package hari1;
+
+public class Hari18 {
+    public static void main(String[] args) {
+        //BELAJAR TIPE DATA
+        // Deklarasi variabel dengan tipe data String
+        String nama = "Arfina";
+        String status = "Mahasiswa";
+        
+        // Deklarasi variabel dengan tipe data integer
+        int umur = 20;
+        
+        // Deklarasi variabel dengan tipe data double
+        double tinggi = 160.0;
+        double berat_badan = 50.0;
+
+        // Menampilkan biodata ke layar
+        System.out.println("Biodata Saya");
+        System.out.println("Nama: " + nama);
+        System.out.println("Status: " + status);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+        System.out.println("Berat badan: " + berat_badan + " kg");
+    }
+    
+}
