79d3ce9ede87ec6752fedc0c43c48768413a9288
ARFINA93 <118178950+ARFINA93@users.noreply.github.com>
2023-10-29 15:14:50 +0800

    Add files via upload

---
+++ Hari21.java
@@ -0,0 +1,14 @@
+package hari1;
+
+public class Hari21 {
+    public static void main(String[] args) {
+        //belajar perulangan while
+        int a = 1;
+        while(a < 31){ //ini adalah pernyataan yang akan dieksekusi selama nilai a kurang dari 31
+            System.out.println("Angka ke-" + a); //perintah yang akan diulang
+            a++; // increment variabel
+        }
+        
+    }
+    
+}
